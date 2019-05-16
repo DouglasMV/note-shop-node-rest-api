c5d96adefb21746587078896fd95c60f6dfc4660
Douglas Mariano Valero <douglasmv@ymail.com>
2019-05-16 11:31:03 -0300

    up to video 7

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://douglas:'+ process.env.MONGO_ATLAS_PW +'@note-shop-node-rest-api-jjlme.mongodb.net/test?retryWrites=true',
+{
+    useNewUrlParser: true
+});
+mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,6 +24,7 @@ app.use((req,res,next) => {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
         return res.status(200).json({});
     }
+    next();
 });
 
 app.use('/products', productRoutes);
