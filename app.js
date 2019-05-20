39ff1e5ec91fd23ea2f9be890aa1bfe88ea003ea
Douglas Mariano Valero <douglasmv@ymail.com>
2019-05-20 09:11:25 -0300

    up to last video

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 //mongoose.connect('mongodb+srv://douglas:'+ process.env.MONGO_ATLAS_PW +'@note-shop-node-rest-api-jjlme.mongodb.net/test?retryWrites=true',
 //{
@@ -20,6 +21,7 @@ mongoose.connect('mongodb://localhost:27017/note-shop-node-rest-api',
 mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/upload', express.static('upload'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -35,6 +37,7 @@ app.use((req,res,next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
